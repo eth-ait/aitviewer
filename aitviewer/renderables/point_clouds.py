Make the AMASS z_up a Node rotation to fix the joint orientation bug.

aitviewer/renderables/point_clouds.py
@@ -27,7 +27,7 @@ class PointClouds(Node):
     Draw a point clouds man!
     """
 
-    def __init__(self, points, colors=None, point_size=5.0, color=(0.0, 0.0, 1.0, 1.0), **kwargs):
+    def __init__(self, points, colors=None, point_size=5.0, color=(0.0, 0.0, 1.0, 1.0), z_up=False, **kwargs):
         """
         A sequence of point clouds. Each point cloud can have a varying number of points.
         :param points: Sequence of points (F, P, 3)
@@ -47,6 +47,9 @@ class PointClouds(Node):
         self.max_n_points = max([p.shape[0] for p in self.points])
 
         self.vao = VAO("points", mode=moderngl.POINTS)
+        
+        if z_up:
+            self.rotation =  np.matmul(np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]]), self.rotation)
 
     @property
     def points(self):

examples/load_AMASS.py
@@ -38,7 +38,8 @@ if __name__ == '__main__':
     # illumination model on the point clouds).
     #
     # Move the point cloud a bit along the x-axis so it doesn't overlap with the mesh data.
-    ptc_amass = PointClouds(seq_amass.vertices, position=np.array([1.0, 0.0, 0.0]), color=c)
+    # Amass data need to be rotated to get the z axis up.
+    ptc_amass = PointClouds(seq_amass.vertices, position=np.array([1.0, 0.0, 0.0]), color=c, z_up=True)
 
     # Display in the viewer.
     v = Viewer()