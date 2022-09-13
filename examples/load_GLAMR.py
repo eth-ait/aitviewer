697929ff79c1ff59181942cd4d2197bc6ff06416
ramenguy99 <ramenguy99@gmail.com>
2022-09-13 17:08:03 +0200

    GLAMR example

---
+++ examples/load_GLAMR.py
@@ -0,0 +1,88 @@
+from aitviewer.viewer import Viewer
+from aitviewer.headless import HeadlessRenderer
+from aitviewer.scene.camera import OpenCVCamera, WeakPerspectiveCamera
+from aitviewer.renderables.billboard import Billboard
+from aitviewer.renderables.smpl import SMPLSequence
+from aitviewer.models.smpl import SMPLLayer
+from aitviewer.configuration import CONFIG as C
+
+import numpy as np
+import pickle
+import os
+import re
+
+
+if __name__ == '__main__':
+    # Create the viewer, set a size that has 16:9 aspect ratio to match the input data
+    viewer = Viewer(size=(1600, 900))
+
+    # Load camera and SMPL data from the output of the GLAMR dynamic camera demo from https://github.com/NVlabs/GLAMR
+    data = pickle.load(open("resources/glamr/running_seed1.pkl", 'rb'))
+
+    person = data['person_data'][0]
+    pose = person['smpl_pose']
+    betas = person['smpl_beta']
+    trans = person['root_trans_world']
+    ori = person['smpl_orient_world']
+
+    # Instantiate an SMPL sequence using the parameters from the data file.
+    # We set z_up=True because GLAMR data is using z_up coordinates.
+    smpl_layer = SMPLLayer(model_type='smpl', gender='neutral', device=C.device)
+    c = (149/255, 149/255, 149/255, 0.8)
+    smpl_sequence = SMPLSequence(poses_body=pose,
+                                 poses_root=ori,
+                                 trans_root=trans,
+                                 betas=betas,
+                                 is_rigged=False,
+                                 smpl_layer=smpl_layer,
+                                 color=c,
+                                 z_up=True)
+
+    # Draw an outline around the SMPL mesh.
+    smpl_sequence.mesh_seq.draw_outline = True
+
+    # Transform y_up coordinates to z_up coordinates.
+    z_up_from_y_up =  np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, 0], [0, 0, 0, 1]], np.float32).T
+
+    # Camera extrinsics expect z up data, we transform each frame to expect y up data instead
+    # since this is the coordinate system used by the viewer.
+    Rt = data['cam_pose']
+    for i in range(Rt.shape[0]):
+        Rt[i] = Rt[i]  @ z_up_from_y_up
+    K = person['cam_K']
+
+
+    # Create a sequence of cameras from camera extrinsics and intrinsics.
+    cols, rows = 3840, 2160
+    camera = OpenCVCamera(K, Rt[:, :3, :], cols, rows, viewer=viewer)
+
+    # Path to the directory containing the video frames.
+    images_path = "resources/glamr/frames"
+
+    # Sort images by frame number in the filename.
+    regex = re.compile(r"(\d*)$")
+    def sort_key(x):
+        name = os.path.splitext(x)[0]
+        return int(regex.search(name).group(0))
+
+    # Create a billboard
+    billboard = Billboard.from_camera_and_distance(camera, 3.0, cols, rows,
+                    [os.path.join(images_path, f) for f in sorted(os.listdir(images_path), key=sort_key)])
+
+    # Add all objects to the scene.
+    viewer.scene.add(smpl_sequence, billboard, camera)
+
+    # Show a visualization of the camera position and orientation over time.
+    camera.show_path()
+
+    # Set initial camera position and target
+    viewer.scene.camera.position = np.array((11.0, 4.0, -9))
+    viewer.scene.camera.target = np.array((-3.3, 0.6, -7.3))
+
+    # Viewer settings
+    viewer.scene.floor.enabled = False
+    viewer.playback_fps = 30.0
+    viewer.shadows_enabled = False
+    viewer.auto_set_camera_target = False
+
+    viewer.run()
---
+++ examples/resources/glamr/README.md
@@ -0,0 +1,8 @@
+## Acknowledgements
+
+Data from the GLAMR dynamic camera demo available at https://github.com/NVlabs/GLAMR
+
+> GLAMR: Global Occlusion-Aware Human Mesh Recovery with Dynamic Cameras  
+> [Ye Yuan](https://www.ye-yuan.com/), [Umar Iqbal](http://www.umariqbal.info/), [Pavlo Molchanov](https://research.nvidia.com/person/pavlo-molchanov/), [Kris Kitani](http://www.cs.cmu.edu/~kkitani/), [Jan Kautz](https://jankautz.com/)  
+> **CVPR 2022 (Oral)**  
+> [website](https://nvlabs.github.io/GLAMR) | [paper](https://arxiv.org/abs/2112.01524) | [video](https://youtu.be/wpObDXcYueo)
