8516d9e0cd308754dcf5420ec457298c1173a0c8
Dario Mylonopoulos <ramenguy99@gmail.com>
2022-10-03 17:21:14 +0200

    Animation and missing_frames exmaple.

---
+++ examples/animation.py
@@ -0,0 +1,71 @@
+from aitviewer.renderables.meshes import Meshes
+from aitviewer.viewer import Viewer
+from aitviewer.utils.so3 import aa2rot_numpy
+
+import numpy as np
+import trimesh
+
+if __name__ == '__main__':
+    # Number of frames.
+    N = 200
+
+    # Load a simple untextured cube.
+    cube = trimesh.load('resources/cube.obj')
+
+    # Create sequences of position, scale and rotation values.
+    p1 = np.linspace(np.array([0, 0, 0]), np.array([5, 0, 0]), num=N)
+    p2 = np.linspace(np.array([0, 0, 0]), np.array([0, 0, 5]), num=N)
+    p3 = np.linspace(np.array([0, 0, 0]), np.array([0, 5, 0]), num=N)
+    r2 = aa2rot_numpy(np.linspace([0, 0, 0], np.array([0, np.pi, 0]), num=N))
+    s3 = np.linspace(1.0, 2.0, num=N)
+
+    # Create 3 cubes, specifying sequences for their position, rotation and/or scale.
+    # Each property can be either a single value (e.g. a single position of shape (3)) or an array
+    # of values (e.g. an array of positions with shape (N, 3) where N is the number of frames).
+    # Array properties will be animated when playing the sequence (you can start playing pressing the spacebar).
+    c1 = Meshes(cube.vertices, cube.faces, name='C1', position=p1, color=(0.5, 0, 0, 1), flat_shading=True)
+    c2 = Meshes(cube.vertices, cube.faces, name='C2', position=p2 + [3.0, 0.0, 0.0], rotation=r2, color=(0.3, 0, 0, 1), flat_shading=True)
+    c3 = Meshes(cube.vertices, cube.faces, name='C3', position=p3 + [3.0, 0.0, 0.0], scale=s3, color=(0.1, 0.1, 0.1, 1), flat_shading=True)
+
+
+    # Some properties of renderable objects can also be animated, such as the vertices of a mesh.
+    # Here we create an array of vertices and vertex colors with shape (N, V, 3) and (N, V, 4) respectively.
+
+    # Load a simple sphere.
+    sphere = trimesh.load('resources/planet/planet.obj')
+
+    # Create initial and final vertex positions.
+    vertices_begin = sphere.vertices
+    vertices_end = sphere.vertices.copy()
+    for i in range(3):
+        # Clamp vertices in each 3D direction preserving the sign to create a cube from the sphere.
+        vertices_begin[:, i] = np.sign(vertices_end[:, i]) * np.minimum(np.abs(vertices_begin[:, i]), np.full((vertices_begin.shape[0]), 1.7))
+    # Linearly interpolate vertex positions.
+    vertices = np.linspace(vertices_begin, vertices_end, N)
+
+    # Linearly interpolate vertex colors.
+    vertex_colors_begin = np.tile(np.array([0, 0, 0.5, 1]), (vertices.shape[1], 1))
+    vertex_colors_end = np.tile(np.array([0, 0.5, 0.0, 1]), (vertices.shape[1], 1))
+    vertex_colors = np.linspace(vertex_colors_begin, vertex_colors_end, N)
+
+    # Create the node with the vertices and colors we computed.
+    cubesphere = Meshes(vertices, sphere.faces, name='CubeSphere', position=[-5, 2, 0], scale=0.5, vertex_colors=vertex_colors, flat_shading=True)
+
+    # Create a viewer.
+    v = Viewer()
+
+    # Set the camera position.
+    v.scene.camera.position = (0, 10, 20)
+
+    # Add the parent object to the scene.
+    v.scene.add(c1)
+
+    # Add the other two cubes as children of the first and second cube respectively creating a hierarchy of 3 nodes.
+    # The transform (position, rotation and scale) of each node is also applied to all children.
+    c1.add(c2)
+    c2.add(c3)
+
+    # Add the animated mesh to the scene.
+    v.scene.add(cubesphere)
+
+    v.run()
---
+++ examples/missing_frames.py
@@ -0,0 +1,74 @@
+from aitviewer.viewer import Viewer
+from aitviewer.headless import HeadlessRenderer
+from aitviewer.renderables.smpl import SMPLSequence
+from aitviewer.models.smpl import SMPLLayer
+from aitviewer.configuration import CONFIG as C
+from aitviewer.utils.so3 import aa2rot_numpy
+
+import joblib
+import numpy as np
+
+if __name__ == '__main__':
+    # In this example we show a way to play sequences of data that has missing frames.
+    # Here we assume that we have data only for the frames that exist but we also know the indices
+    # of missing frames and we want to avoid padding our data with invalid frames.
+    # Instead, we create a mask that has ones for each frame for which we have data and zeros
+    # everywhere else. Thus the number of ones in the mask must match the number of frames in the data.
+
+    # Load SMPL data from the output of the VIBE demo from https://github.com/mkocabas/VIBE
+    data = joblib.load(open("resources/vibe/vibe_output.pkl", 'rb'))
+    poses = data[1]['pose']
+    betas = data[1]['betas']
+
+    # Number of total frames, including frames that are missing.
+    N = poses.shape[0]
+
+    # Number of existing frames of data. We are going to remove 100 frames to create holes in our data.
+    M = N - 100
+
+    # Create a hole of 100 frames in the data, we now have M frames of data.
+    # In this example we suppose that we have 25 frames of data followed by a hole of 100 frames.
+    p = np.concatenate((poses[:25], poses[125:]))
+    b = np.concatenate((betas[:25], betas[125:]))
+
+    # Create a mask with N frames.
+    enabled_frames = np.ones(N, dtype=np.bool8)
+    # Set to zero the values at the indices of the 100 missing frames. There are now only M ones in the mask.
+    enabled_frames[25:125] = 0
+
+
+    # Instantiate an SMPL sequence using the parameters and the enabled_frames mask.
+    smpl_layer = SMPLLayer(model_type='smpl', gender='neutral', device=C.device)
+    smpl_seq = SMPLSequence(poses_body=p[:, 3:24 * 3],
+                            poses_root=p[:, 0:3],
+                            betas=b,
+                            smpl_layer=smpl_layer,
+                            position=(1, 0, 0),
+                            rotation=aa2rot_numpy(np.array([1, 0, 0]) * np.pi),
+                            enabled_frames=enabled_frames)
+
+    # We do the same for a second sequence but removing a different range of frames.
+    # Notice how the two sequences remain in sync, since the sequence is only advanced
+    # during frames that are enabled (where the mask value is one).
+    p2 = np.concatenate((poses[:175], poses[275:]))
+    b2 = np.concatenate((betas[:175], betas[275:]))
+    enabled_frames2 = np.ones(N, dtype=np.bool8)
+    enabled_frames2[175:275] = 0
+    smpl_layer2 = SMPLLayer(model_type='smpl', gender='neutral', device=C.device)
+    smpl_seq2 = SMPLSequence(poses_body=p2[:, 3:24 * 3],
+                            poses_root=p2[:, 0:3],
+                            betas=b2,
+                            smpl_layer=smpl_layer2,
+                            position=(-1, 0, 0),
+                            rotation=aa2rot_numpy(np.array([1, 0, 0]) * np.pi),
+                            enabled_frames=enabled_frames2)
+
+    # Run the viewer
+    viewer = Viewer(size=(1600, 900))
+    viewer.scene.floor.enabled = False
+    viewer.scene.camera.position = (0, 1, 5)
+    viewer.scene.fps = 25
+    viewer.playback_fps = 25
+    viewer.scene.add(smpl_seq, smpl_seq2)
+    viewer.run()
+
