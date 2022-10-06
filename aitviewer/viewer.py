2dcee2173e0238b7cbb0bcf442895556597db908
Dario Mylonopoulos <ramenguy99@gmail.com>
2022-10-06 17:23:22 +0200

    Config option for window type

--- aitviewer/viewer.py
+++ aitviewer/viewer.py
@@ -72,7 +72,6 @@ SHORTCUTS = {
 
 class Viewer(moderngl_window.WindowConfig):
     resource_dir = Path(__file__).parent / 'shaders'
-    window_type = 'pyqt5'
     size_mult = 1.0
     samples = 4
     gl_version = (4, 0)
@@ -90,7 +89,7 @@ class Viewer(moderngl_window.WindowConfig):
             C.update_conf(config)
 
         # Window Setup (Following `moderngl_window.run_window_config`).
-        base_window_cls = get_local_window_cls(self.window_type)
+        base_window_cls = get_local_window_cls(C.window_type)
 
         # If no size is provided use the size from the configuration file
         if size is None:
