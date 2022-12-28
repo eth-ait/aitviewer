9f163c75402ff9b068673a983a39c59f63ec1865
Dario Mylonopoulos <ramenguy99@gmail.com>
2022-12-28 19:38:11 +0100

    Updated remote.py example

--- examples/remote.py
+++ examples/remote.py
@@ -2,21 +2,10 @@ from aitviewer.remote.viewer import RemoteViewer
 from aitviewer.remote.renderables.meshes import Meshes
 
 import trimesh
-from time import sleep
 
 cube = trimesh.load('resources/cube.obj')
 
-if False:
-    v = RemoteViewer("localhost")
-    m = Meshes(cube.vertices, cube.faces, flat_shading=True, name=f"Cube", position=(5, 0, 0))
-    v.scene.add(m)
-    v.close()
-else:
-    v = RemoteViewer("10.0.0.1")
-    m = Meshes(cube.vertices, cube.faces, flat_shading=True, name=f"Cube", position=(5, 0, 0))
-
-    # m.append(cube.vertices)
-
-    v.scene.add(m)
-    m.append(cube.vertices)
-    v.close()
+v = RemoteViewer()
+m = Meshes(cube.vertices, cube.faces, flat_shading=True, name=f"Cube", position=(5, 0, 0))
+v.scene.add(m)
+v.close()
