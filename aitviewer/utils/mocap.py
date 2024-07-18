2c792695410932d4851be265467f1005ef4769ab
MarilynKeller <marilyn.keller@tue.mpg.de>
2024-07-18 17:57:26 +0200

    Add SKEL, BSM, Markers and Bioamass support and examples.

---
+++ aitviewer/utils/mocap.py
@@ -0,0 +1,92 @@
+# # Code Developed by Marilyn Keller, marilyn.keller@tuebingen.mpg.de
+# # Do not share or distribute without permission of the author
+import numpy as np
+import tqdm
+
+
+
+import c3d
+import numpy as np
+import tqdm
+
+
+
+try:
+    import nimblephysics as nimble
+except ImportError:
+    raise("nimblephysics not found. Please install nimblephysics with 'pip install nimblephysics' to use this module.")
+
+def clean_CMU_mocap_labels(c3dFile: nimble.biomechanics.C3D):
+    "Rename all the labels with the pattern AAAA-XX and replace them by AAAA"
+
+    c3dFile.markers = [name for name in c3dFile.markers if "-" not in name]
+
+    markerTimesteps = c3dFile.markerTimesteps.copy()
+
+    for markers_dict in markerTimesteps:
+        markers_dict_clean = markers_dict.copy()
+        for key in markers_dict:
+            if "-" in key:
+                key_clean = key.split("-")[0]
+                markers_dict_clean[key_clean] = markers_dict_clean.pop(key)
+        markers_dict.clear()
+        markers_dict.update(markers_dict_clean)
+
+    c3dFile.markerTimesteps = markerTimesteps
+
+    return c3dFile
+
+
+def load_markers(c3d_path, nb_markers_expected=None):
+    # Load the marker trajectories
+    import os
+    assert os.path.exists(c3d_path), f"File {c3d_path} not found."
+    try:
+        import nimblephysics as nimble
+    except:
+        raise ImportError("Please install nimblephysics to load c3d files")
+    
+    try:
+        c3dFile: nimble.biomechanics.C3D = nimble.biomechanics.C3DLoader.loadC3D(os.path.abspath(c3d_path))
+    except Exception as e:
+        print(f"Error loading c3d file {c3d_path}: {e}")
+        raise e
+    
+    c3dFile = clean_CMU_mocap_labels(c3dFile)
+
+    # This c3dFile.markerTimesteps is cryptonite, it keeps doing weird stuff (aka changing values, or you can not edit it),
+    # it behaves normaly if you make a copy
+    markers_data_list = c3dFile.markerTimesteps.copy()
+    
+
+    markers_labels = c3dFile.markers
+    markers_labels.sort()
+    nb_markers = len(markers_labels)
+
+    if nb_markers_expected is not None:
+        assert len(markers_labels) == nb_markers_expected, "Expected {} markers, found {}".format(
+            nb_markers_expected, len(markers_labels)
+        )
+    print(f"Found {nb_markers} markers: {markers_labels}")
+
+    # List of per frame pc array
+    markers_array = np.zeros((len(markers_data_list), nb_markers, 3))  # FxMx3
+    for frame_id, marker_data in (pbar := tqdm.tqdm(enumerate(markers_data_list))):
+        pbar.set_description("Generating markers point clouds ")
+        for marker_id, marker_name in enumerate(markers_labels):
+            if marker_name in marker_data:
+                marker_pos = marker_data[marker_name]
+                if np.any(np.abs(marker_pos) > 10e2):
+                    print(
+                        "Warning: marker {} is too far away on frame {}, will be displayed in (0,0,0)".format(
+                            marker_name, frame_id
+                        )
+                    )
+                    marker_pos = np.nan * np.zeros((3))
+            else:
+                marker_pos = np.nan * np.zeros((3))
+            markers_array[frame_id, marker_id, :] = marker_pos
+            
+    fps = c3dFile.framesPerSecond
+    
+    return markers_array, markers_labels, fps
---
+++ examples/load_SKEL.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2024 Max Planck Institute for Intelligent Systems, Marilyn Keller, marilyn.keller@tuebingen.mpg.de
+
+import argparse
+import os
+
+import numpy as np
+import torch
+
+from aitviewer.configuration import CONFIG as C
+from aitviewer.renderables.skel import SKELSequence
+from aitviewer.viewer import Viewer
+
+try:
+    from skel.skel_model import SKEL
+except Exception as e:
+    print("Could not import SKEL, make sure you installed the skel repository.")
+    raise e
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Load a SKEL model and display it.")
+    parser.add_argument("-s", "--motion_file", type=str, help="Path to a skel motion file", default=None)
+    parser.add_argument("-z", "--z_up", help="Rotate the mesh 90 deg", action="store_true")
+
+    args = parser.parse_args()
+
+    skel_model = SKEL(gender="female", model_path=C.skel_models).to(C.device)
+
+    if args.motion_file is None:
+        F = 120
+        pose = torch.zeros(F, 46).to(C.device)
+        betas = torch.zeros(F, 10).to(C.device)
+        betas[: F // 2, 0] = torch.linspace(-2, 2, F // 2)  # Vary beta0 between -2 and 2
+        betas[F // 2 :, 1] = torch.linspace(-2, 2, F // 2)  # Vary beta1 between -2 and 2
+
+        trans = torch.zeros(F, 3).to(C.device)
+
+        # Test SKEL forward pass
+        skel_output = skel_model(pose, betas, trans)
+
+        skel_seq = SKELSequence(
+            skel_layer=skel_model,
+            betas=betas,
+            poses_body=pose,
+            poses_type="skel",
+            trans=trans,
+            is_rigged=True,
+            show_joint_angles=True,
+            name="SKEL",
+            z_up=False,
+            skin_coloring="skinning_weights",  # "pose_offsets", "skinning_weights"
+        )
+        cam_pose = None
+
+    else:
+
+        assert os.path.exists(
+            args.motion_file
+        ), f"Could not find {args.motion_file}, please provide a valid path to a skel motion file."
+        assert args.motion_file.endswith(".pkl"), f"Please provide a .pkl file."
+
+        skel_seq = SKELSequence.from_pkl(args.motion_file, name="SKEL", fps_in=120, fps_out=30, z_up=args.z_up)
+        cam_pose = np.array([0, 1.2, 4.0])
+
+    v = Viewer()
+    v.playback_fps = 30
+    v.scene.add(skel_seq)
+    v.run_animations = True
+    if cam_pose is not None:
+        v.scene.camera.position = cam_pose
+    v.run()
